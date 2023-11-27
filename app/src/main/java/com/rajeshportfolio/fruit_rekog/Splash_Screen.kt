b7800d7dfaa4168fa6b5ff15dbabe07d9e4c90a9
kanugurajesh <245122733172@mvsrec.edu.in>
2023-11-27 17:38:23 +0530

    Adding the splash screen

---
+++ app/src/main/java/com/rajeshportfolio/fruit_rekog/Splash_Screen.kt
@@ -0,0 +1,25 @@
+package com.rajeshportfolio.fruit_rekog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(500)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
